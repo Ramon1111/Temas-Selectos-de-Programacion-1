2e5db033ec07ca250fab93c7d404798e71f2a607
Ramon1111 <jramoncito7-99@hotmail.com>
2020-03-02 14:56:03 -0600

    [FEAT] Work with fragments with navbar menu

--- MetDidactico/app/src/main/java/com/example/metdidactico/LoginActivity.java
+++ MetDidactico/app/src/main/java/com/example/metdidactico/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.metdidactico;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -62,8 +61,10 @@ public class LoginActivity extends AppCompatActivity {
     View.OnClickListener onClickCrearCuenta=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intentSignUp=new Intent(getApplicationContext(), MainActivity.class);
+            Intent intentSignUp=new Intent(getApplicationContext(), RegistroActivity.class);
             startActivity(intentSignUp);
+            finish();
+            //Usualmente no se sugiere que siga corriendo la activity pero hay veces que sí xd.
         }
     };
 
--- MetDidactico/app/src/main/java/com/example/metdidactico/MainActivity.java
+++ MetDidactico/app/src/main/java/com/example/metdidactico/MainActivity.java
@@ -1,64 +1,15 @@
 package com.example.metdidactico;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import java.util.EventListener;
-
-public class MainActivity extends AppCompatActivity {
-
-    EditText edtNombre, edtCorreo, edtPassword;
-    Button btnCrear;
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Se ejecuta al incio.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Los recursos se cuguardan en un archivo R por id que es un dato nimérico
-        edtNombre=findViewById(R.id.edtNombreMain);
-        edtCorreo=findViewById(R.id.edtCorreoMain);
-        edtPassword=findViewById(R.id.edtContrasenaMain);
-        btnCrear=findViewById(R.id.btnCrearMain);
-
-        btnCrear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String Nombre=edtNombre.getText().toString();
-                String Correo=edtCorreo.getText().toString();
-                String Password=edtPassword.getText().toString();
-                if(Nombre.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Escriba su nombre", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    if(Correo.isEmpty()){
-                        Toast.makeText(getApplicationContext(), "Escriba un correo", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        if(Password.isEmpty()){
-                            Toast.makeText(getApplicationContext(), "Escriba una contraseña", Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(getApplicationContext(), "Se ha registrado con éxito", Toast.LENGTH_SHORT).show();
-
-                            //Para mandar a la siguiente página
-                            //También se usa para pasar parámetros por lo que se usa un método
-                            Intent intentlogin=new Intent(getApplicationContext(), LoginActivity.class);
-                            intentlogin.putExtra(getString(R.string.mainStringHintCorreo),Correo);
-                            intentlogin.putExtra(getString(R.string.mainStringHintContrasena),Password);
-                            startActivity(intentlogin);
-                        }
-                    }
-                }
-            }
-        });
     }
 }
